80e64a43cddf27fcb536d9cfc2a1103a26727e40
Javi Marasco <javier.marasco@gmail.com>
2022-03-16 21:38:03 +0100

    Split all writing logic in a separate file

--- write.go
+++ write.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager/types"
 )
 
-func WriteSecret(keyvault_name string, path string, secret secret_struct, creds *auth) {
-	base_uri := fmt.Sprint("https://", keyvault_name, ".vault.azure.net")
+func WriteSecret(path string, secret secret_struct, creds *auth) {
+	base_uri := fmt.Sprint("https://", creds.KeyVault, ".vault.azure.net")
 	inputForHash := path + "+" + secret.Name
 	secretName := CreateHash(inputForHash)
 
@@ -59,4 +65,40 @@ func WriteSecret(keyvault_name string, path string, secret secret_struct, creds 
 		fmt.Println("Get failed with error: ", resp)
 	}
 
+}
+
+func WriteAWSSecret(path string, region string, secretname string, secretvalue string) {
+	inputForHash := path + "+" + secretname
+	secretNameHashed := CreateHash(inputForHash)
+
+	var newtags = make(map[string]string)
+	newtags = PathToTags(path)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	conn := secretsmanager.NewFromConfig(cfg, func(o *secretsmanager.Options) {
+		o.Region = region
+	})
+
+	tags := []types.Tag{}
+	for k, v := range newtags {
+		temptag := types.Tag{Key: aws.String(k), Value: aws.String(v)}
+		tags = append(tags, temptag)
+	}
+	temptag := types.Tag{Key: aws.String("SecretName"), Value: aws.String(secretname)}
+	tags = append(tags, temptag)
+
+	input := secretsmanager.CreateSecretInput{
+		Name:         aws.String(secretNameHashed),
+		SecretString: aws.String(secretvalue),
+		Tags:         tags,
+	}
+	output, err := conn.CreateSecret(context.TODO(), &input)
+	if err != nil {
+		fmt.Println("Error writing secret.")
+	}
+	fmt.Println(output)
 }
