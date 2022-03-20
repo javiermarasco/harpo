36bfa606afca50ab99a6832adcfbe52b05288b38
Javi Marasco <javier.marasco@gmail.com>
2022-03-20 16:01:34 +0100

    Support to delete secrets in AWS and AZ

---
+++ delete.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+func DeleteAzSecret(path string, secret secret_struct, creds *auth) {
+	base_uri := fmt.Sprint("https://", creds.KeyVault, ".vault.azure.net")
+	inputForHash := path + "+" + secret.Name
+	secretName := CreateHash(inputForHash)
+
+	uri := fmt.Sprint(base_uri, "/secrets/", secretName, "?api-version=7.2")
+
+	// DELETE
+	req, err := http.NewRequest("DELETE", uri, nil)
+	req.Header.Add("Authorization", fmt.Sprint("bearer ", creds.Token))
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		_, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Successfully deleted secret from Azure Keyvault")
+	} else {
+		fmt.Println("Delete failed with error: ", resp)
+	}
+}
+
+func DeleteAwsSecret(path string, secretname string) {
+	region := os.Getenv("AWS_REGION")
+	inputForHash := path + "+" + secretname
+	secretNameHashed := CreateHash(inputForHash)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn := secretsmanager.NewFromConfig(cfg, func(o *secretsmanager.Options) {
+		o.Region = region
+	})
+
+	deletesecret := secretsmanager.DeleteSecretInput{
+		SecretId: aws.String(secretNameHashed),
+	}
+	_, err = conn.DeleteSecret(context.TODO(), &deletesecret)
+	if err != nil {
+		fmt.Println("Error deleting secret.")
+	}
+}
